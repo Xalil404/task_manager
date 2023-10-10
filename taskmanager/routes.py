7d195503bc23a1aaa6561b25adb627a0888d1f08
TEST <test@TESTs-MacBook-Pro.local>
2023-10-10 10:59:01 +0100

    Created database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -3,6 +3,10 @@ from taskmanager import app, db
 from taskmanager.models import Category, Task
 
 
+app_ctx = app.app_context()
+app_ctx.push()
+
+
 @app.route("/")
 def home():
     tasks = list(Task.query.order_by(Task.id).all())
